0151b260f7e72e29eefe34bbbd2d1670811b7b7e
OEM <oem@OEMs-MacBook-Pro.local>
2018-02-14 10:21:05 -0800

    changed db config

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'mysql://b921826d6bcb34:9bc3157c@us-cdbr-iron-east-05.cleardb.net/heroku_59b3ba477891df1?reconnect=true'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'support-ticket'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', 'Summer1996**'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
