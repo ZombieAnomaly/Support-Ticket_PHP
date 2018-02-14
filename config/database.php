5a139e5ca67a468961e7f3d5e1652bb7c6c65ace
OEM <oem@OEMs-MacBook-Pro.local>
2018-02-14 12:42:28 -0800

    Tickets are now stored and emails are logged

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-if(!empty($url)["host"]){
+if(!empty($url["host"])){
     $host = $url["host"];
     $username = $url["user"];
     $password = $url["pass"];
