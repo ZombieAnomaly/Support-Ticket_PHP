c837d127a943a0fe72df12c5498cb8db137aae8c
OEM <oem@OEMs-MacBook-Pro.local>
2018-02-22 10:36:06 -0800

    added a few simple feature tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,18 +4,69 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\libs\DataBaseInterfaces\TicketsI;
+use App\libs\DataBaseInterfaces\CommentsI;
+use App\libs\DataBaseInterfaces\CategoryI;
+use App\libs\DataBaseInterfaces\VotesI;
+use Illuminate\Container\Container;
+
+
 
 class ExampleTest extends TestCase
 {
+   
     /**
      * A basic test example.
      *
      * @return void
      */
+
+    
+
     public function testBasicTest()
     {
         $response = $this->get('/');
+        $response->assertRedirect('/login');
+       // $response->assertStatus(200);
+    }
 
-        $response->assertStatus(200);
+    public function testUnitTickets()
+    {
+        $container = Container::getInstance();
+        $instance = $container->make(TicketsI::class);
+        $this->assertTrue(
+            $instance->getTickets()
+            ->get()
+            ->contains('id', 1)
+        );
+        $this->assertFalse(
+            $instance->getTickets()
+            ->get()
+            ->contains('id', 0)
+        );
+    }
+
+    public function testUnitVotes()
+    {
+        $container = Container::getInstance();
+        $instance = $container->make(VotesI::class);
+        
+        $this->assertTrue(
+            $instance->getVotes()
+            ->contains('id', 1)
+        );
+
+        $this->assertNotEmpty(
+            $instance->getTopVotes()
+        );
+ 
+        $this->assertNotEmpty(
+            $instance->Totalvotes('SPHCL7JFOF')
+        );
+        
+        $this->assertFalse(
+            $instance->getVotes()
+            ->contains('id', 0)
+        );
     }
 }
